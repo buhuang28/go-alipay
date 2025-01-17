3b2a0be97f1fab7f7ecdf615009e3858b667ccfb
Lin <shulin2050@vip.qq.com>
2025-01-17 10:57:44 +0800

    1:改成SDK

--- main.go
+++ main.go
@@ -1,18 +1,5 @@
 package main
 
-import (
-	"github.com/buhuang28/go-alipay/config"
-	"github.com/buhuang28/go-alipay/db"
-	"github.com/buhuang28/go-alipay/logs"
-)
-
-func init() {
-	logs.LogInit()
-	c := config.LoadConfig("./config.json")
-	db.DbInit(c.DBConfig)
-
-}
-
 func main() {
 
 }
